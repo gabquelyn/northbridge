c5fc16511c2120b67ec406d44dbae6014b6c228f
Arebamen <gabquelyn@gmail.com>
2026-06-06 18:01:42 +0100

    Fixed the program selection

--- app/components/form/CanadianSelection.tsx
+++ app/components/form/CanadianSelection.tsx
@@ -83,7 +83,6 @@ function CanadianSelection({
                   type="radio"
                   name="canadian"
                   value={select.value}
-                  className="hidden"
                   disabled={isDisabled}
                   checked={isSelected}
                   onChange={(e) => {
