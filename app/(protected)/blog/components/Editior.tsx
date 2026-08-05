65597fbae060919b50e8881049fa349440bcb8e8
Arebamen Gabriel <gabquelyn@gmail.com>
2026-08-05 16:04:47 +0100

    editor scroll change

--- app/(protected)/blog/components/Editior.tsx
+++ app/(protected)/blog/components/Editior.tsx
@@ -337,7 +337,10 @@ export default function RichTextEditor({
         </ToolbarButton>
       </div>
 
-      <div className="flex-1 overflow-y-auto px-5 py-4" style={{ minHeight }}>
+      <div
+        className="editor-scroll flex-1 overflow-y-auto px-5 py-4"
+        style={{ minHeight }}
+      >
         <EditorContent
           editor={editor}
           className="
